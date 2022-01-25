5b2cf633c3200098d6f91977281c55fb5aa6e21a
alinashev <al.t.shevchenko@gmail.com>
2022-01-25 08:21:36 +0200

    Added downloading files from s3

---
+++ Commons/StorageS3.py
@@ -0,0 +1,30 @@
+import os
+from typing import Any
+
+import boto3
+import botocore
+
+from Settings import awsSettings
+
+
+class StorageS3:
+    def __init__(self, bucket_name='task-bucket-a') -> None:
+        self.s3 = None
+        self.bucket_name = bucket_name
+        self.connect()
+
+    def connect(self, access_key_id=awsSettings.access_key_id,
+                secret_access_key=awsSettings.secret_access_key):
+
+        self.s3 = boto3.resource('s3',
+                                 aws_access_key_id=access_key_id,
+                                 aws_secret_access_key=secret_access_key)
+
+    def download_file(self, obj_name, file_name):
+        try:
+            self.s3.Bucket(self.bucket_name).download_file(obj_name, file_name)
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                print("The object does not exist.")
+            else:
+                raise
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from Commons.StorageS3 import StorageS3
+
+
 def main():
-    pass
+    channel_id = "UC05WaqA-0OLM5Y7NmmHq1hw"
+    storage = StorageS3(bucket_name="a-test-01-bucket")
+    storage.download_file("Resources/2022-01-24/20h/channelData/" + "chd" + channel_id + ".json",
+                          "chd" + channel_id + ".json")
+
+    storage.download_file("Resources/2022-01-24/20h/videoData/" + "vd" + channel_id + ".json",
+                          "vd" + channel_id + ".json")
+
+    storage.download_file("Resources/2022-01-24/20h/categoryData/" + "ctd" + channel_id + ".json",
+                          "ctd" + channel_id + ".json")
 
 
 if __name__ == '__main__':
